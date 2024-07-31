d4bb00cf71c3de0534405c6b9a548b064b3ee05b
Satria Alipatullah <satrialipatullah@gmail.com>
2024-07-31 10:08:07 +0700

    render wallet address

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,7 +1,10 @@
 "use client";
 import useWalletClient from "@/hooks/useWalletClient";
+import { useState } from "react";
+import { type Address } from "viem";
 export default function Header() {
   const { requestAddresses, addChain } = useWalletClient();
+  const [add, setAdd] = useState<Address | null>(null);
   return (
     <header className="flex justify-center text-white p-8">
       <div className="max-w-7xl flex justify-between w-full">
@@ -13,20 +16,32 @@ export default function Header() {
             </div>
           ))}
         </nav>
-        <button
-          className="text-[#F8C200]"
-          type="button"
-          onClick={async () => {
-            try {
-              const [address] = await requestAddresses();
-              console.log(address);
-            } catch (error) {
-              console.log("connect wallet err:", error);
-            }
-          }}
-        >
-          connect wallet
-        </button>
+        <div className="">
+          {add === null ? (
+            <button
+              className="text-[#F8C200]"
+              type="button"
+              onClick={async () => {
+                try {
+                  const [address] = await requestAddresses();
+                  setAdd(address);
+                  console.log(address);
+                } catch (error) {
+                  console.log("connect wallet err:", error);
+                }
+              }}
+            >
+              connect wallet
+            </button>
+          ) : (
+            <button
+              type="button"
+              className="bg-[#F8C200] p-2 rounded-md overflow-hidden w-[100px]"
+            >
+              <span className="">{add}</span>
+            </button>
+          )}
+        </div>
       </div>
     </header>
   );
