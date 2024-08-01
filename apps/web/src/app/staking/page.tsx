5cd8ae19a57127a19dc8e89ef57b392d29b1e9fd
Satria Alipatullah <satrialipatullah@gmail.com>
2024-08-01 19:58:13 +0700

    add: project list

--- apps/web/src/app/staking/page.tsx
+++ apps/web/src/app/staking/page.tsx
@@ -1,4 +1,5 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Switch } from "@/components/ui/switch";
 
 export default function StakingPage() {
   return (
@@ -66,17 +67,66 @@ export default function StakingPage() {
       </section>
       <section className="flex justify-center mt-[100px]">
         <div className="w-full max-w-7xl flex flex-col items-center">
-          <Tabs defaultValue="account" className="w-[400px]-">
-            <TabsList>
-              <TabsTrigger value="account">Account</TabsTrigger>
-              <TabsTrigger value="password">Password</TabsTrigger>
-            </TabsList>
-            <span className="">sakdjsak</span>
-            <TabsContent value="account">
-              Make changes to your account here.
+          <Tabs defaultValue="live" className="w-[400px]- w-full">
+            <div className="flex mb-16">
+              <TabsList className="grid grid-cols-2 w-fit bg-[#242424] p-2 h-fit">
+                <TabsTrigger value="live" className="px-16">
+                  LIVE
+                </TabsTrigger>
+                <TabsTrigger value="finished" className="px-16">
+                  FINISHED
+                </TabsTrigger>
+              </TabsList>
+              <span className="w-full flex items-center ml-8 justify-between">
+                <div className="flex items-center gap-2">
+                  <Switch className="bg-[#242424]" />
+                  <span className="">My Staking Pool</span>
+                </div>
+                <div className="bg-[#F8C200] text-[20px] text-black px-16 py-2 h-full rounded-sm">
+                  STAKING
+                </div>
+              </span>
+            </div>
+            <TabsContent value="live">
+              <div className="space-y-2">
+                {Array.from({ length: 5 }).map((d, i) => (
+                  <div
+                    key={`d${d}`}
+                    className="flex gap-2 items-center justify-between p-4 rounded-md bg-[#242424]"
+                  >
+                    <span className="flex h-12 w-12 aspect-square rounded-sm bg-yellow-400" />
+                    <p className="">Community Raise of Hand</p>
+                    <div className="text-center">
+                      <h1 className="text-[10px] text-[#9A9A9A]">
+                        Total Raise
+                      </h1>
+                      <p className="text-[14px] font-medium">$4000</p>
+                    </div>
+                    <div className="text-center">
+                      <h1 className="text-[10px] text-[#9A9A9A]">
+                        PARTICIPANT
+                      </h1>
+                      <p className="text-[14px] font-medium">100</p>
+                    </div>
+                    <div className="text-center">
+                      <h1 className="text-[10px] text-[#9A9A9A]">STATUS</h1>
+                      <p className="text-[14px] font-medium text-[#F8C200]">
+                        FINISHED
+                      </p>
+                    </div>
+                    <button
+                      type="button"
+                      className="bg-[#9A9A9A] rounded-sm text-black px-12 py-2"
+                    >
+                      CLAIM
+                    </button>
+                  </div>
+                ))}
+              </div>
             </TabsContent>
-            <TabsContent value="password">
-              Change your password here.
+            <TabsContent value="finished">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque vel
+              mollitia quod libero labore nisi eligendi et. Soluta, aut rerum.
             </TabsContent>
           </Tabs>
         </div>
