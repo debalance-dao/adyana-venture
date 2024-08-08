1a15b59d1be6c017d5330d034ee8c7291e7226de
Satria Alipatullah <satrialipatullah@gmail.com>
2024-08-08 22:49:00 +0700

    fix: prevent blank screen when there's no wallet installed

--- apps/web/src/config/viem.ts
+++ apps/web/src/config/viem.ts
@@ -1,38 +1,36 @@
 import {
-	createWalletClient,
-	custom,
-	createPublicClient,
-	type Account,
+  createWalletClient,
+  custom,
+  createPublicClient,
+  type Account,
 } from "viem";
 import { polygon } from "viem/chains";
 
 declare global {
-	interface Window {
-		ethereum: unknown;
-	}
+  interface Window {
+    ethereum: unknown;
+  }
 }
 export let walletClient: ReturnType<typeof createWalletClient>;
 export let publicClient: ReturnType<typeof createPublicClient>;
 type EthereumProvider = {
-	request: (args: { method: string; params?: unknown[] }) => Promise<unknown>;
+  request: (args: { method: string; params?: unknown[] }) => Promise<unknown>;
 };
 export let ethereum: EthereumProvider;
 
 if (globalThis.window) {
-	ethereum = window?.ethereum as EthereumProvider;
-	const requestAccount = async () => {
-		const [account] = (await ethereum?.request({
-			method: "eth_requestAccounts",
-		})) as Account[];
-		return account;
-	};
+  ethereum = window?.ethereum as EthereumProvider;
 
-	publicClient = createPublicClient({
-		chain: polygon,
-		transport: custom(ethereum),
-	});
-	walletClient = createWalletClient({
-		chain: polygon,
-		transport: custom(ethereum),
-	});
+  if (ethereum !== undefined) {
+    publicClient = createPublicClient({
+      chain: polygon,
+      transport: custom(ethereum),
+    });
+    walletClient = createWalletClient({
+      chain: polygon,
+      transport: custom(ethereum),
+    });
+  } else {
+    alert("metamask not installed!");
+  }
 }
